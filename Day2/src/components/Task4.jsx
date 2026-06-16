ae25e589e3462bbb8b987579f10a52ba97dd282d
DEVA22ad <deva22ad@gmail.com>
2026-06-16 10:30:25 +0530

    Day 2 Task 4: Fetch API

---
+++ Day2/src/components/Task4.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error: {error}</h2>;
+  }
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>User Data Fetch and Display</h1>
+
+      {users.map((user) => (
+        <div
+          key={user.id}
+          style={{
+            border: "1px solid #ccc",
+            padding: "10px",
+            margin: "10px 0",
+            borderRadius: "8px",
+          }}
+        >
+          <h3>{user.name}</h3>
+          <p>Email: {user.email}</p>
+          <p>Company: {user.company.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
