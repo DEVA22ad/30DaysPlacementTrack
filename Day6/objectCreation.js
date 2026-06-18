6b21576a5297b5a32a69b941f3cf2fc05961023b
DEVA22ad <deva22ad@gmail.com>
2026-06-18 12:39:44 +0530

    Day-6:Object creation

---
+++ Day6/objectCreation.js
@@ -0,0 +1,12 @@
+
+const student = {
+    name: "Deva",
+    age: 22,
+    course: "MERN Stack",
+    city: "Chennai"
+};
+
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("City:", student.city);
